8d4638d420948f75387ab2e17f2054f0e0bea628
Hein Andre Grønnestad <hag@haugnett.no>
2018-10-11 16:23:02 +0200

    Initial commit 2

---
+++ NextionFontEditor/NextionFontEditor/Form1.Designer.cs
@@ -0,0 +1,109 @@
+﻿namespace NextionFontEditor
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.p = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.p2 = new System.Windows.Forms.PictureBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.p)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.p2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // p
+            // 
+            this.p.Location = new System.Drawing.Point(12, 12);
+            this.p.Name = "p";
+            this.p.Size = new System.Drawing.Size(335, 551);
+            this.p.TabIndex = 0;
+            this.p.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 641);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 586);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 2;
+            // 
+            // p2
+            // 
+            this.p2.Location = new System.Drawing.Point(353, 12);
+            this.p2.Name = "p2";
+            this.p2.Size = new System.Drawing.Size(512, 551);
+            this.p2.TabIndex = 3;
+            this.p2.TabStop = false;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(276, 586);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(949, 682);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.p2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.p);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.p)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.p2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox p;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.PictureBox p2;
+        private System.Windows.Forms.TextBox textBox2;
+    }
+}
+
---
+++ NextionFontEditor/NextionFontEditor/Form1.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace NextionFontEditor
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var file16 = @"C:\Users\hag\Desktop\fontello_16.zi";
+            var file32 = @"C:\Users\hag\Desktop\fontello_32.zi";
+
+            var bytes16 = File.ReadAllBytes(file16);
+            var bytes32 = File.ReadAllBytes(file32);
+
+            textBox1.Text = Encoding.ASCII.GetString(bytes16.Skip(0x1C).Take(bytes16[0x14]).ToArray());
+            textBox2.Text = Encoding.ASCII.GetString(bytes32.Skip(0x1C).Take(bytes32[0x14]).ToArray());
+
+            //DrawFont(bytes16, p);
+            DrawFont(bytes32, p2);
+        }
+
+        private void DrawFont(byte[] bytes, PictureBox p)
+        {
+            var cWidth = bytes[6];
+            var cHeight = bytes[7];
+            var spacing = 12;
+            var g = p.CreateGraphics();
+
+            var bb = new SolidBrush(Color.Black);
+            var pr = new Pen(Color.Red);
+
+            var columns = 15;
+            var rows = 1;
+
+            for (int row = 0; row < rows; row++)
+            {
+                for (int column = 0; column < columns; column++)
+                {
+                    var cell = (row * columns) + column;
+                    var bytesPerLine = cWidth / 8;
+                    var charData = bytes.Skip(0x24 + (cell * cHeight * bytesPerLine)).Take(cHeight * bytesPerLine).ToArray();
+
+                    for (int charLine = 0; charLine < cHeight; charLine++)
+                    {
+                        g.DrawRectangle(pr, (column * cWidth) + (column * spacing), row * (cHeight + spacing), cWidth, cHeight);
+
+                        var lineBytes = charData.Skip(bytesPerLine * charLine).Take(bytesPerLine).ToArray();
+
+                        for (int lineByte = 0; lineByte < lineBytes.Length; lineByte++)
+                        {
+                            for (int k = 0; k < 8; k++)
+                            {
+                                if ((lineBytes[lineByte] & (1 << k)) != 0)
+                                {
+                                    var x = (column * (cWidth + spacing)) + (bytesPerLine * lineByte) + k;
+                                    var y = (row * (cHeight + spacing)) + charLine;
+
+                                    g.FillRectangle(bb, x, y, 1, 1);
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
