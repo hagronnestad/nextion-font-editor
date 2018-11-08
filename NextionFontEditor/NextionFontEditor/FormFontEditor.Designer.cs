5adadb0b337ea99371cf759df5f209138a9b34dc
Hein Andre Grønnestad <hag@haugnett.no>
2018-11-09 00:21:13 +0100

    Created UI for CharEditor with grid toggle and zoom control

--- NextionFontEditor/NextionFontEditor/FormFontEditor.Designer.cs
+++ NextionFontEditor/NextionFontEditor/FormFontEditor.Designer.cs
@@ -24,65 +24,218 @@
         /// </summary>
         private void InitializeComponent() {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormFontEditor));
-            this.charEditor1 = new NextionFontEditor.Controls.CharEditor();
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pPreview = new System.Windows.Forms.PictureBox();
             this.numChar = new System.Windows.Forms.NumericUpDown();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.btnOpenFont = new System.Windows.Forms.ToolStripButton();
+            this.ofd = new System.Windows.Forms.OpenFileDialog();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.charEditor1 = new NextionFontEditor.Controls.CharEditor();
+            this.tsCharEditor = new System.Windows.Forms.ToolStrip();
+            this.cmbZoom = new System.Windows.Forms.ToolStripComboBox();
+            this.toolStripLabel1 = new System.Windows.Forms.ToolStripLabel();
+            this.btnShowGrid = new System.Windows.Forms.ToolStripButton();
+            this.panelWrapper = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pPreview)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.numChar)).BeginInit();
+            this.toolStrip1.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.tsCharEditor.SuspendLayout();
+            this.panelWrapper.SuspendLayout();
             this.SuspendLayout();
             // 
-            // charEditor1
+            // pPreview
             // 
-            this.charEditor1.CharImage = ((System.Drawing.Bitmap)(resources.GetObject("charEditor1.CharImage")));
-            this.charEditor1.Location = new System.Drawing.Point(12, 162);
-            this.charEditor1.Name = "charEditor1";
-            this.charEditor1.ShowGrid = true;
-            this.charEditor1.Size = new System.Drawing.Size(240, 480);
-            this.charEditor1.TabIndex = 0;
-            this.charEditor1.Text = "charEditor1";
-            this.charEditor1.Zoom = 30;
-            this.charEditor1.Click += new System.EventHandler(this.charEditor1_Click);
-            // 
-            // pictureBox1
-            // 
-            this.pictureBox1.Location = new System.Drawing.Point(12, 57);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(240, 86);
-            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
-            this.pictureBox1.TabIndex = 1;
-            this.pictureBox1.TabStop = false;
+            this.pPreview.BackColor = System.Drawing.Color.White;
+            this.pPreview.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pPreview.Location = new System.Drawing.Point(12, 106);
+            this.pPreview.Name = "pPreview";
+            this.pPreview.Size = new System.Drawing.Size(100, 100);
+            this.pPreview.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.pPreview.TabIndex = 1;
+            this.pPreview.TabStop = false;
             // 
             // numChar
             // 
             this.numChar.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.numChar.Location = new System.Drawing.Point(12, 12);
+            this.numChar.Location = new System.Drawing.Point(12, 54);
             this.numChar.Name = "numChar";
-            this.numChar.Size = new System.Drawing.Size(122, 30);
+            this.numChar.Size = new System.Drawing.Size(100, 30);
             this.numChar.TabIndex = 2;
             this.numChar.ValueChanged += new System.EventHandler(this.numChar_ValueChanged);
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.btnOpenFont});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 0);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(553, 25);
+            this.toolStrip1.TabIndex = 3;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // btnOpenFont
+            // 
+            this.btnOpenFont.Image = ((System.Drawing.Image)(resources.GetObject("btnOpenFont.Image")));
+            this.btnOpenFont.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.btnOpenFont.Name = "btnOpenFont";
+            this.btnOpenFont.Size = new System.Drawing.Size(83, 22);
+            this.btnOpenFont.Text = "Open Font";
+            this.btnOpenFont.Click += new System.EventHandler(this.btnOpenFont_Click);
+            // 
+            // ofd
+            // 
+            this.ofd.Filter = "Nextion Font Files|*.zi";
+            // 
+            // panel1
+            // 
+            this.panel1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.panel1.AutoScroll = true;
+            this.panel1.AutoScrollMargin = new System.Drawing.Size(20, 20);
+            this.panel1.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.panel1.Controls.Add(this.charEditor1);
+            this.panel1.Location = new System.Drawing.Point(0, 25);
+            this.panel1.Margin = new System.Windows.Forms.Padding(0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(415, 427);
+            this.panel1.TabIndex = 4;
+            this.panel1.Resize += new System.EventHandler(this.panel1_Resize);
+            // 
+            // charEditor1
+            // 
+            this.charEditor1.CharImage = ((System.Drawing.Bitmap)(resources.GetObject("charEditor1.CharImage")));
+            this.charEditor1.Location = new System.Drawing.Point(53, 36);
+            this.charEditor1.Margin = new System.Windows.Forms.Padding(20);
+            this.charEditor1.Name = "charEditor1";
+            this.charEditor1.ShowGrid = true;
+            this.charEditor1.Size = new System.Drawing.Size(160, 320);
+            this.charEditor1.TabIndex = 0;
+            this.charEditor1.Text = "charEditor1";
+            this.charEditor1.Zoom = 10;
+            this.charEditor1.Click += new System.EventHandler(this.charEditor1_Click);
+            // 
+            // tsCharEditor
+            // 
+            this.tsCharEditor.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.btnShowGrid,
+            this.cmbZoom,
+            this.toolStripLabel1});
+            this.tsCharEditor.Location = new System.Drawing.Point(0, 0);
+            this.tsCharEditor.Name = "tsCharEditor";
+            this.tsCharEditor.RenderMode = System.Windows.Forms.ToolStripRenderMode.System;
+            this.tsCharEditor.Size = new System.Drawing.Size(413, 25);
+            this.tsCharEditor.TabIndex = 1;
+            this.tsCharEditor.Text = "toolStrip2";
+            // 
+            // cmbZoom
+            // 
+            this.cmbZoom.Alignment = System.Windows.Forms.ToolStripItemAlignment.Right;
+            this.cmbZoom.AutoSize = false;
+            this.cmbZoom.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmbZoom.Name = "cmbZoom";
+            this.cmbZoom.Size = new System.Drawing.Size(50, 23);
+            this.cmbZoom.SelectedIndexChanged += new System.EventHandler(this.cmbZoom_SelectedIndexChanged);
+            this.cmbZoom.TextChanged += new System.EventHandler(this.cmbZoom_TextChanged);
+            // 
+            // toolStripLabel1
+            // 
+            this.toolStripLabel1.Alignment = System.Windows.Forms.ToolStripItemAlignment.Right;
+            this.toolStripLabel1.Name = "toolStripLabel1";
+            this.toolStripLabel1.Size = new System.Drawing.Size(39, 22);
+            this.toolStripLabel1.Text = "Zoom";
+            // 
+            // btnShowGrid
+            // 
+            this.btnShowGrid.Checked = true;
+            this.btnShowGrid.CheckOnClick = true;
+            this.btnShowGrid.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.btnShowGrid.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.btnShowGrid.Image = ((System.Drawing.Image)(resources.GetObject("btnShowGrid.Image")));
+            this.btnShowGrid.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.btnShowGrid.Name = "btnShowGrid";
+            this.btnShowGrid.Size = new System.Drawing.Size(23, 22);
+            this.btnShowGrid.Text = "Show Grid";
+            this.btnShowGrid.Click += new System.EventHandler(this.btnShowGrid_Click);
+            // 
+            // panelWrapper
+            // 
+            this.panelWrapper.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.panelWrapper.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panelWrapper.Controls.Add(this.tsCharEditor);
+            this.panelWrapper.Controls.Add(this.panel1);
+            this.panelWrapper.Location = new System.Drawing.Point(126, 28);
+            this.panelWrapper.Name = "panelWrapper";
+            this.panelWrapper.Size = new System.Drawing.Size(415, 452);
+            this.panelWrapper.TabIndex = 5;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(9, 38);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(53, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Character";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(9, 90);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(45, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Preview";
             // 
             // FormFontEditor
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1141, 765);
+            this.ClientSize = new System.Drawing.Size(553, 492);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.panelWrapper);
+            this.Controls.Add(this.toolStrip1);
             this.Controls.Add(this.numChar);
-            this.Controls.Add(this.pictureBox1);
-            this.Controls.Add(this.charEditor1);
+            this.Controls.Add(this.pPreview);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "FormFontEditor";
             this.Text = "Font Editor";
             this.Load += new System.EventHandler(this.FormFontEditor_Load);
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.Shown += new System.EventHandler(this.FormFontEditor_Shown);
+            ((System.ComponentModel.ISupportInitialize)(this.pPreview)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.numChar)).EndInit();
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            this.panel1.ResumeLayout(false);
+            this.tsCharEditor.ResumeLayout(false);
+            this.tsCharEditor.PerformLayout();
+            this.panelWrapper.ResumeLayout(false);
+            this.panelWrapper.PerformLayout();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private Controls.CharEditor charEditor1;
-        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pPreview;
         private System.Windows.Forms.NumericUpDown numChar;
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripButton btnOpenFont;
+        private System.Windows.Forms.OpenFileDialog ofd;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.ToolStrip tsCharEditor;
+        private System.Windows.Forms.ToolStripComboBox cmbZoom;
+        private System.Windows.Forms.ToolStripLabel toolStripLabel1;
+        private System.Windows.Forms.ToolStripButton btnShowGrid;
+        private System.Windows.Forms.Panel panelWrapper;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
     }
 }
--- NextionFontEditor/NextionFontEditor/FormFontEditor.cs
+++ NextionFontEditor/NextionFontEditor/FormFontEditor.cs
@@ -14,21 +14,55 @@ namespace NextionFontEditor {
         private ZiFont ziFont;
 
         private void FormFontEditor_Load(object sender, EventArgs e) {
-            ziFont = ZiFont.FromFile(@"Test Files\Arial_16_ascii.zi");
-
-            numChar.Maximum = ziFont.CodePage.CharacterCount - 1;
-
-            charEditor1.CharImage = ziFont.CharBitmaps.Skip(1).First();
-            numChar.Value = 1;
+            cmbZoom.Items.AddRange(Enumerable.Range(1, 30).Select(x => $"{x}x").ToArray());
+            cmbZoom.SelectedIndex = 9;
         }
 
         private void charEditor1_Click(object sender, EventArgs e) {
-            pictureBox1.Image = charEditor1.CharImage;
+            pPreview.Image = charEditor1.CharImage;
         }
 
         private void numChar_ValueChanged(object sender, EventArgs e) {
             charEditor1.CharImage = ziFont.CharBitmaps.Skip((int) numChar.Value).First();
-            pictureBox1.Image = charEditor1.CharImage;
+            pPreview.Image = charEditor1.CharImage;
+        }
+
+        private void openToolStripMenuItem_Click(object sender, EventArgs e) {
+
+        }
+
+        private void btnOpenFont_Click(object sender, EventArgs e) {
+            var res = ofd.ShowDialog();
+
+            if (res == DialogResult.OK) {
+                ziFont = ZiFont.FromFile(ofd.FileName);
+
+                numChar.Maximum = ziFont.CodePage.CharacterCount - 1;
+
+                charEditor1.CharImage = ziFont.CharBitmaps.Skip(1).First();
+                numChar.Value = 1;
+            }
+        }
+
+        private void panel1_Resize(object sender, EventArgs e) {
+            charEditor1.Left = (panel1.Width / 2) - (charEditor1.Width / 2);
+            charEditor1.Top = (panel1.Height / 2) - (charEditor1.Height / 2);
+        }
+
+        private void FormFontEditor_Shown(object sender, EventArgs e) {
+            panel1_Resize(sender, e);
+        }
+
+        private void cmbZoom_TextChanged(object sender, EventArgs e) {
+        }
+
+        private void cmbZoom_SelectedIndexChanged(object sender, EventArgs e) {
+            charEditor1.Zoom = int.Parse(cmbZoom.Text.Replace("x", ""));
+            panel1_Resize(sender, e);
+        }
+
+        private void btnShowGrid_Click(object sender, EventArgs e) {
+            charEditor1.ShowGrid = btnShowGrid.Checked;
         }
     }
 }
