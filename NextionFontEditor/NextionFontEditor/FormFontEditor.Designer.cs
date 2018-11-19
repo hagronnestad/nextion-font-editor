6dfe5300c3c92c58a7bdeaba70b1e3b813e986ac
Hein Andre Grønnestad <hag@haugnett.no>
2018-11-19 23:08:00 +0100

    Fixed merge

--- NextionFontEditor/NextionFontEditor/FormFontEditor.Designer.cs
+++ NextionFontEditor/NextionFontEditor/FormFontEditor.Designer.cs
@@ -273,7 +273,6 @@
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(553, 536);
-            this.Controls.Add(this.flowPanel);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.panelWrapper);
