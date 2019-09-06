309c9389000abc92cc8fa0860ed395547a3556f9
fvanroie <fvanroie@users.noreply.github.com>
2019-09-07 00:00:59 +0200

    Changed to IZICharacter interface and revert back to List insead of Array.

--- NextionFontEditor/NextionFontEditor/FormFontEditor.cs
+++ NextionFontEditor/NextionFontEditor/FormFontEditor.cs
@@ -1,7 +1,7 @@
 ﻿using System;
 using System.Linq;
 using System.Windows.Forms;
-using ZiLib.FileVersion.V3;
+using ZiLib.FileVersion.Common;
 
 namespace NextionFontEditor {
 
@@ -33,19 +33,19 @@ namespace NextionFontEditor {
                 return;
             }
 
-            if (numChar.Value >= ziFont.Characters.Length) {
+            if (numChar.Value >= ziFont.Characters.Count) {
                 numChar.Value = 0;
             }
 
             if (numChar.Value < 0) {
-                numChar.Value = ziFont.Characters.Length - 1;
+                numChar.Value = ziFont.Characters.Count - 1;
             }
 
-            var bmp = ziFont.Characters[(int)numChar.Value].GetBitmap();
+            var bmp = ziFont.Characters[(int)numChar.Value].ToBitmap();
             if (bmp != null && bmp.PixelFormat != System.Drawing.Imaging.PixelFormat.Undefined)
             {
-                charEditor1.CharImage = bmp;
-                pPreview.Image = charEditor1.CharImage;
+                charEditor1.Character = ziFont.Characters[(int)numChar.Value];
+                pPreview.Image = bmp;
             } else {
             }
             txtEncoding.Text = ziFont.CodePage.CodePageIdentifier.GetDescription();
@@ -115,9 +115,10 @@ namespace NextionFontEditor {
         }
 
         private void charEditor1_Paint(object sender, PaintEventArgs e) {
-            if (charEditor1.CharImage.PixelFormat != System.Drawing.Imaging.PixelFormat.Undefined)
+            var CharImage = charEditor1.Character?.ToBitmap();
+            if (CharImage?.PixelFormat != System.Drawing.Imaging.PixelFormat.Undefined)
             {
-                pPreview.Image = charEditor1.CharImage;
+                pPreview.Image = CharImage;
             }
             else
             {
@@ -152,7 +153,7 @@ namespace NextionFontEditor {
 
         private void btnCopy_Click(object sender, EventArgs e)
         {
-            ziFont.Characters[(int)numChar.Value].CopyToClipboard();
+            ZiClipboard.CopyToClipboard(ziFont.Characters[(int)numChar.Value]);
         }
 
         private void btnDeleteCharacter_Click(object sender, EventArgs e)
@@ -187,7 +188,8 @@ namespace NextionFontEditor {
 
                     for (ushort ch = 32; ch <= 127; ch++) {
                         if (ziFont.CodePage.CodePoints.Contains(ch)) {
-                            if (Array.Exists(ziFont.Characters, character => { return character.CodePoint == ch; })) {
+                            if (ziFont.Characters.Exists(character => { return character.CodePoint == ch; }))
+                            {
                                 if (true) {
                                     // replace existing char
                                 }
@@ -196,7 +198,8 @@ namespace NextionFontEditor {
                                 var txt = Char.ConvertFromUtf32((int)ch);
                                 var bmp = ZiLib.Extensions.BitmapExtensions.DrawString(txt, "", (byte)ziFont.CharacterHeight);
                                 var bytes = ZiLib.FileVersion.V5.BinaryTools.BitmapTo3BppData(bmp);
-                                ziFont.AddCharacter(ch, bytes, (byte)bmp.Width);
+                                var character = ZiCharacter.FromBytes(ziFont, ch, bytes, (byte)bmp.Width,0,0);
+                                ziFont.AddCharacter(ch,character);
                             }
                         }
                     }
