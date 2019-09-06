a42613c0d5a06daf4acfc40d3bfab43102b9783c
fvanroie <fvanroie@users.noreply.github.com>
2019-09-06 21:10:32 +0200

    removed comments.

--- NextionFontEditor/NextionFontEditor/FormFontEditor.cs
+++ NextionFontEditor/NextionFontEditor/FormFontEditor.cs
@@ -201,13 +201,6 @@ namespace NextionFontEditor {
                         }
                     }
 
-
-                    //string val = form.ReturnValue1;            //values preserved after close
-                    //string dateString = form.ReturnValue2;
-                    //Do something here with these values
-
-                    //for example
-                    //this.txtSomething.Text = val;
                 }
             }
         }
