5d05da568f799ca159d486f9323730e510030a82
fvanroie <fvanroie@users.noreply.github.com>
2019-09-11 20:38:46 +0200

    Fixed some >16 Mb file issues. Removed unused code.

--- NextionFontEditor/ZiLib/FileVersion/V5/ZiFontV5.cs
+++ NextionFontEditor/ZiLib/FileVersion/V5/ZiFontV5.cs
@@ -44,7 +44,6 @@ namespace ZiLib.FileVersion.V5 {
 
         public void Save(string fileName, CodePage codePage) {
             CodePage = codePage;
-            //_charData = CreateCharData(CharBitmaps);
             Save(fileName);
         }
 
@@ -83,37 +82,39 @@ namespace ZiLib.FileVersion.V5 {
             // Update Character Data and Character Indexes
             //CharacterCount = (uint)Characters.Length;
             var offset = (uint)CharacterCount * 10u;
+            var align8bytes = (byte)0x00;
             for (int i = 0; i < CharacterCount; i++)
             {
                 bytes = Characters[i].GetCharacterData();
+                indexData.AddRange(GetCharacterIndex(Characters[i], offset, bytes.Length));
                 charData.AddRange(bytes);
-                indexData.AddRange( GetCharacterIndex(Characters[i], offset, bytes.Length) );
                 offset += (uint)bytes.Length;
             }
             VariableDataLength = (uint)(offset + FileNameLength + encodingNameLength);
 
-            // V6 8 byte aligned charData for file bigger than 16 MB
+            // V6 8 byte aligned charData for files bigger than 16 MB
             if (offset > 16 * 1024 * 1024) {
                 Version = 6;
                 byte[] paddingbytes = new byte[] { 0, 0, 0, 0, 0, 0, 0, 0 };
-                charData.Clear();
 
+                indexData.Clear();
+                charData.Clear();
                 offset = (uint)CharacterCount * 10u;
-                var paddinglength = (int)offset % 8;
-                charData.AddRange(paddingbytes.Take(paddinglength));
-                offset = (uint)((bytes.Length + paddinglength) >> 3);
+                align8bytes = (byte)0x01;
 
                 for (int i = 0; i < CharacterCount; i++) {
-                    bytes = Characters[i].GetCharacterData();
-                    charData.AddRange(bytes);
-
-                    paddinglength = bytes.Length % 8;
+                    /* Pad the previous data */
+                    var paddinglength = (8 - (int)(offset % 8)) % 8;
                     charData.AddRange(paddingbytes.Take(paddinglength));
+                    offset += (uint)paddinglength;
 
-                    indexData.AddRange(GetCharacterIndex(Characters[i], offset, bytes.Length));
-                    offset += (uint)((bytes.Length + paddinglength) >> 3);
+                    /* Add next character */
+                    bytes = Characters[i].GetCharacterData();
+                    indexData.AddRange(GetCharacterIndex(Characters[i], (uint)(offset / 8), bytes.Length));
+                    charData.AddRange(bytes);
+                    offset += (uint)bytes.Length;
                 }
-                VariableDataLength = (uint)(offset*8 + FileNameLength + encodingNameLength);
+                VariableDataLength = (uint)(offset + FileNameLength + encodingNameLength);
             }
 
             file.AddRange(MagicNumbers);
@@ -152,7 +153,7 @@ namespace ZiLib.FileVersion.V5 {
             file.Add(0x01); // AntiAlias
             file.Add(0x01); // VariableWidth
             file.Add(FileNameLength);
-            file.Add(0x00); // 0x01 : Chardata is 8 byte aligned or 0x00 chardata is 1 byte aligned
+            file.Add(align8bytes); // 0x01 : Chardata is 8 byte aligned or 0x00 chardata is 1 byte aligned
 
             file.AddRange(BitConverter.GetBytes((ushort)0)); // Reserved 2 bytes
 
@@ -201,24 +202,16 @@ namespace ZiLib.FileVersion.V5 {
             var codePageId = ziFont._header[0x4];
             var characterCount = BitConverter.ToUInt32(ziFont._header.Skip(0x0C).Take(4).ToArray(), 0);
             ziFont.Characters = new List<IZiCharacter>();
-
             ziFont.CodePage = new CodePage((CodePageIdentifier) codePageId);
 
             var charMapData = bytes.Skip(HEADER_LENGTH + ziFont.FileNameLength).Take(10 * (int)characterCount).ToArray();
+            var align8byte = bytes.Skip(33).First();
 
             for (int i = 0; i < charMapData.Length; i += 10) {
-                /*
-                var c = new CharMapEntry {
-                    Code = BitConverter.ToUInt16(charMapData, i),
-                    Width = charMapData[i + 2],
-                    KerningLeft = charMapData[i + 3],
-                    KerningRight = charMapData[i + 4],
-                    DataAddressOffset = BitConverter.ToUInt32(new byte[] { charMapData[i + 5], charMapData[i + 6], charMapData[i + 7], 0x00 }, 0),
-                    Length = BitConverter.ToUInt16(charMapData, i + 8)
-                };
-                ziFont.CharacterEntries.Add(c);
-                */
                 var dataAddressOffset = BitConverter.ToUInt32(new byte[] { charMapData[i + 5], charMapData[i + 6], charMapData[i + 7], 0x00 }, 0);
+                if (align8byte == 1) {
+                    dataAddressOffset *= 8;
+                }
                 var dataLength = BitConverter.ToUInt16(charMapData, i + 8);
 
                 var data = new byte[dataLength];
@@ -237,220 +230,6 @@ namespace ZiLib.FileVersion.V5 {
 
                 //Debug.WriteLine($"i: {i} code: {ch.CodePoint} width: {ch.GetBitmap().Width} dataOffset: n/a length: {ch.GetCharacterData().Length}");
             }
-
-            //ziFont.CreateBitmaps();
-
-            return ziFont;
-        }
-
-        /*private void CreateBitmaps()
-        {
-            CharBitmaps.Clear();
-
-            for (ushort charIndex = 0; charIndex < CharacterCount; charIndex++)
-            {
-
-                var charData = bytes.Skip(HEADER_LENGTH + FileNameLength + (int)CharacterEntries[charIndex].DataAddressOffset).Take(CharacterEntries[charIndex].Length).ToArray();
-
-
-                //Kerning[0] = kerningL;
-                //Kerning[1] = kerningR;
-                if ((CharacterEntries[charIndex].TotalWidth == 0) || (CharacterHeight == 0))
-                {
-                    var bm = new Bitmap(1, 1);
-                    bm.Dispose();           // Make it undefined initially
-                    CharBitmaps.Add(bm);
-                    continue;              // to next character
-                }
-                var b = new Bitmap(CharacterEntries[charIndex].TotalWidth, CharacterHeight);
-                //b.Tag = charData;
-
-                var pixelNumber = 0;
-                var bitdepth = charData.First();
-
-                foreach (var item in charData.Skip(1))
-                {
-                    var drawingMode = item >> 6;
-                    var drawingMode2 = (item & 0b00100000) >> 5;
-                    var number = (item & 0b00011111);
-
-                    // B & W
-                    //if (charDataChar2[0] == 1) {
-
-                    if (drawingMode == 0)
-                    {
-
-                        if (drawingMode2 == 0)
-                        {
-                            for (int p = 0; p < number; p++)
-                            {
-                                b.SetPixelNumber(pixelNumber, Color.Transparent);
-
-                                pixelNumber++;
-                            }
-                        }
-
-                        if (drawingMode2 == 1)
-                        {
-                            for (int p = 0; p < number; p++)
-                            {
-                                b.SetPixelNumber(pixelNumber, Color.Black);
-
-                                pixelNumber++;
-                            }
-                        }
-
-                    }
-
-                    if (drawingMode == 1)
-                    {
-
-                        for (int p = 0; p < number; p++)
-                        {
-                            b.SetPixelNumber(pixelNumber, Color.Transparent);
-
-                            pixelNumber++;
-                        }
-
-                        b.SetPixelNumber(pixelNumber, Color.Black);
-                        pixelNumber++;
-
-                        if (drawingMode2 == 1)
-                        {
-                            b.SetPixelNumber(pixelNumber, Color.Black);
-                            pixelNumber++;
-                        }
-
-                    }
-                    //}
-
-                    //// Alpha
-                    //if (charDataChar2[0] == 3) {
-
-                    if (drawingMode == 2)
-                    {
-
-                        if (bitdepth == 1)
-                        {
-                            for (int p = 0; p < number; p++)
-                            {
-                                b.SetPixelNumber(pixelNumber, Color.Transparent);
-
-                                pixelNumber++;
-                            }
-
-                            b.SetPixelNumber(pixelNumber, Color.Black);
-                            pixelNumber++;
-                            b.SetPixelNumber(pixelNumber, Color.Black);
-                            pixelNumber++;
-                            b.SetPixelNumber(pixelNumber, Color.Black);
-                            pixelNumber++;
-
-                            if (drawingMode2 == 1)
-                            {
-                                b.SetPixelNumber(pixelNumber, Color.Black);
-                                pixelNumber++;
-                            }
-                        }
-                        else
-                        {
-                            number = (item & 0b00111000) >> 3;
-                            var alpha = (item & 0b00000111);
-
-                            for (int p = 0; p < number; p++)
-                            {
-                                b.SetPixelNumber(pixelNumber, Color.Transparent);
-
-                                pixelNumber++;
-                            }
-
-                            b.SetPixelNumber(pixelNumber, Color.FromArgb((255 / 7) * alpha, Color.Black));
-                            pixelNumber++;
-                        }
-                    }
-
-                    if (drawingMode == 3)
-                    {
-
-                        if (bitdepth == 1)
-                        {
-                            var whites = (item & 0b00111000) >> 3;
-                            var blacks = (item & 0b00000111);
-
-                            for (int p = 0; p < whites; p++)
-                            {
-                                b.SetPixelNumber(pixelNumber, Color.Transparent);
-
-                                pixelNumber++;
-                            }
-
-                            for (int p = 0; p < blacks; p++)
-                            {
-                                b.SetPixelNumber(pixelNumber, Color.Black);
-
-                                pixelNumber++;
-                            }
-                        }
-                        else
-                        {
-                            var alpha1 = (item & 0b00111000) >> 3;
-                            var alpha2 = (item & 0b00000111);
-
-                            // should be alpha
-                            b.SetPixelNumber(pixelNumber, Color.FromArgb((255 / 7) * alpha1, Color.Black));
-                            pixelNumber++;
-
-                            // should be alpha
-                            b.SetPixelNumber(pixelNumber, Color.FromArgb((255 / 7) * alpha2, Color.Black));
-                            pixelNumber++;
-                        }
-                    }
-                    //}
-
-                }
-
-                CharBitmaps.Add(b);
-            }
-
-        }
-        */
-        private byte[] CreateCharData(List<Bitmap> characters, bool invertColour = false)
-        {
-            var charData = new List<byte>();
-
-            for (int i = 0; i < CharacterCount; i++)
-            {
-                byte[] bytes = BinaryTools.BitmapTo3BppData(characters[i], invertColour);
-                //CharacterEntries[i].DataAddressOffset = 0;
-                charData.AddRange(bytes);
-            }
-
-            return charData.ToArray();
-        }
-
-
-        public static ZiFontV5 FromCharacterBitmaps(string fontName, byte width, byte height, CodePage codePage, List<Bitmap> characters, bool invertColour = false) {
-            var bytesPerChar = width * height / 8;
-            var charDataLength = (uint) (bytesPerChar * characters.Count());
-
-            var ziFont = new ZiFontV5 {
-                Name = fontName,
-                //FileNameLength = (byte) fontName.Length,
-                CharacterWidth = width,
-                CharacterHeight = height,
-                CodePage = codePage,
-                CharDataLength = charDataLength,
-                VariableDataLength = (uint) fontName.Length + charDataLength,
-            };
-
-            ziFont._charData = ziFont.CreateCharData(characters, invertColour);
-
-            //var charData = new List<byte>();
-            //foreach (var cb in characters) {
-            //    charData.AddRange(BinaryTools.BitmapTo1BppData(cb));
-            //}
-            //ziFont._charData = charData.ToArray();
-
             return ziFont;
         }
 
