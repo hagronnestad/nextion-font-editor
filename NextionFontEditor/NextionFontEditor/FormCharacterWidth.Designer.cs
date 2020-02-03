ac2ff6e9e4b1238b5983f820401dfd66baf560f2
fvanroie <fvanroie@users.noreply.github.com>
2020-02-03 21:45:24 +0100

    Update Form Icons

--- NextionFontEditor/NextionFontEditor/FormCharacterWidth.Designer.cs
+++ NextionFontEditor/NextionFontEditor/FormCharacterWidth.Designer.cs
@@ -23,6 +23,7 @@
         /// the contents of this method with the code editor.
         /// </summary>
         private void InitializeComponent() {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormCharacterWidth));
             this.lblWidth = new System.Windows.Forms.Label();
             this.lblKerningL = new System.Windows.Forms.Label();
             this.lblKerningR = new System.Windows.Forms.Label();
@@ -150,6 +151,7 @@
             this.Controls.Add(this.lblKerningR);
             this.Controls.Add(this.lblKerningL);
             this.Controls.Add(this.lblWidth);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "FormCharacterWidth";
             this.Text = "Character Width";
             ((System.ComponentModel.ISupportInitialize)(this.numWidth)).EndInit();
